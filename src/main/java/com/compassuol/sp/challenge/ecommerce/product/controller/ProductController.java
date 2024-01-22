d99179e161c39f20fe8ce89143e6753cf71dec9b
Jeffleyg <jeffleygarcon007@gmail.com>
2024-01-22 19:27:58 -0300

    Essa parte é para deletar produto por id

--- src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -23,6 +20,11 @@ public class ProductController {
         Product product = productService.createProduct(newProduct);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
-    
 
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id){
+        productService.remove(id);
+        return ResponseEntity.noContent().build();
+    }
 }
