6d7a7111843c2bf7a3d3fbe29d28c3c2e133f137
pablitohaddad <pablohaddad73@gmail.com>
2024-01-22 16:36:21 -0400

    Estrutura básica de criação de produtos

--- src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
@@ -1,5 +1,28 @@
 package com.compassuol.sp.challenge.ecommerce.product.controller;
 
+import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
+import com.compassuol.sp.challenge.ecommerce.product.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product newProduct){
+        Product product = productService.createProduct(newProduct);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+    
 
 }
--- src/main/java/com/compassuol/sp/challenge/ecommerce/product/service/ProductService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/product/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.compassuol.sp.challenge.ecommerce.product.service;
 
+import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
+import com.compassuol.sp.challenge.ecommerce.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public Product createProduct(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
 }
