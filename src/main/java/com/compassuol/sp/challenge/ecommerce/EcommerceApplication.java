8ec26488d4643d45e3e36b3ce9dc9560259676d2
Heitor Machado <90213050+HeitorAxe@users.noreply.github.com>
2024-01-22 15:26:02 -0300

    Initial Commit

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
