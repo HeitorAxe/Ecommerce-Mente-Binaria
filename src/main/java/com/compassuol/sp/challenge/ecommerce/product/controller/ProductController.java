2f52006f05442905397ec8856f015f8a4d4f7301
Jeffleyg <jeffleygarcon007@gmail.com>
2024-01-23 18:49:56 -0300

    Implmentei a parte de Swagger do SpringDoc

--- src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/product/controller/ProductController.java
@@ -7,6 +7,12 @@ import com.compassuol.sp.challenge.ecommerce.product.dto.mapper.ProductMapper;
 import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
 import com.compassuol.sp.challenge.ecommerce.product.repository.projection.ProductProjection;
 import com.compassuol.sp.challenge.ecommerce.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name ="Products", description = "Contains all operations to register, edit, delete, view a product.")
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
@@ -25,12 +32,26 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "Create a new product", description = "Feature to create a new user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))),
+                    @ApiResponse(responseCode = "409", description = "product already registered in the system",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Resource not processed due to invalid input data",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<Product> createProduct(@Valid @RequestBody Product newProduct){
         Product product = productService.createProduct(newProduct);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all products as pageable", description = "Retrieve products as pageable",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "List of products retrieved successfully as pageable",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PageableDTO.class)))
+            })
     @GetMapping("/page")
     public ResponseEntity<PageableDTO> getAllAsPage(@PageableDefault(size = 5)Pageable pageable){
         Page<ProductProjection> projection = productService.getAllAsPage(pageable);
@@ -38,26 +59,51 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "List all registered products", description = "No authentication required",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "List all registered products",
+                            content = @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ProductResponseDTO.class)))),
+                    @ApiResponse(responseCode = "403", description = "User without permission to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping
     public ResponseEntity<List<ProductResponseDTO>> getAll(){
         List<Product> products = productService.getAll();
         return ResponseEntity.ok(ProductMapper.toListDTO(products));
     }
-    
 
+
+
+    @Operation(summary = "Delete a product by ID", description = "No authentication required",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Product deleted successfully"),
+                    @ApiResponse(responseCode = "403", description = "User without permission to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Product not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id){
         productService.remove(id);
         return ResponseEntity.noContent().build();
     }
 
-<<<<<<< HEAD
+
+    @Operation(summary = "Retrieve a product by id", description = "No authentication required",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource retrieved successfully",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponseDTO.class))),
+                    @ApiResponse(responseCode = "403", description = "User without permission to access this resource",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> getById(@PathVariable Long id) {
         Product product = productService.buscarPorId(id);
-        return ResponseEntity.ok(ProductMapper.toDto(product));
+        return ResponseEntity.ok(ProductMapper.toDTO(product));
     }
-=======
->>>>>>> 27d15489f418a4a3e4955b5ec84c7f68f984659c
+
 }
