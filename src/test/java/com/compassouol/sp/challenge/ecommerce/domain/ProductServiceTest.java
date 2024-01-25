b55c1b8f1850c8a0eee27ce2ee41b85db3094a8b
Luiz Henrique <luizhenriquee97@gmail.com>
2024-01-25 16:29:52 -0300

    test: adiciona testes para o Update

--- src/test/java/com/compassouol/sp/challenge/ecommerce/domain/ProductServiceTest.java
+++ src/test/java/com/compassouol/sp/challenge/ecommerce/domain/ProductServiceTest.java
@@ -1,21 +1,24 @@
 package com.compassouol.sp.challenge.ecommerce.domain;
 
 import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
+import com.compassuol.sp.challenge.ecommerce.product.exception.ProductNameUniqueViolationException;
 import com.compassuol.sp.challenge.ecommerce.product.repository.ProductRepository;
 import com.compassuol.sp.challenge.ecommerce.product.service.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
-import static com.compassouol.sp.challenge.ecommerce.common.ProductConstants.INVALID_PRODUCT;
-import static com.compassouol.sp.challenge.ecommerce.common.ProductConstants.PRODUCT;
+import static com.compassouol.sp.challenge.ecommerce.common.ProductConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -60,7 +63,7 @@ class ProductServiceTest {
         //Heitor
     }
 
-    @Test
+   @Test
     void  getProductById_ByNonexistentId_ReturnsEmpty() {
         when(productRepository.findById(1L)).thenReturn(Optional.empty());
         Optional<Product> sut =productService.get(1L);
@@ -70,7 +73,7 @@ class ProductServiceTest {
     @Test
     void  getProductById_ByExistentId_ReturnsProduct() {
         when(productRepository.findById(1L)).thenReturn(Optional.of(PRODUCT));
-        Optional<Product> sut =productService.get(1L);
+        Optional<Product> sut = productService.get(1L);
         assertThat(sut).isNotEmpty();
         assertThat(sut.get()).isEqualTo(PRODUCT);
 
@@ -88,14 +91,23 @@ class ProductServiceTest {
 
     @Test
     void updateProduct_WithValidDatas(){
-        //Luiz
+        when(productRepository.save(PRODUCT)).thenReturn(PRODUCT);
+         productService.updateProduct(PRODUCT);
+         assertThat(PRODUCT).isEqualTo(PRODUCT);
     }
     @Test
-    void updateProduct_WithInvalidId(){
-        //Luiz
+    void updateProduct_WithInvalidId() {
+        when(productRepository.findById(anyLong())).thenThrow(EntityNotFoundException.class);
+
+        assertThatThrownBy(() -> productService.getById(1L)).isInstanceOf(EntityNotFoundException.class);
+
+        verify(productRepository).findById(1L);
     }
+
     @Test
-    void updateProduct_WithInvalidDatas(){
-        //Luiz
+    void updateProduct_WithExistingName_ThrowsException(){
+        when(productRepository.save(PRODUCT)).thenThrow(ProductNameUniqueViolationException.class);
+        assertThatThrownBy(() -> productService.updateProduct(PRODUCT)).isInstanceOf(DataIntegrityViolationException.class);
+
     }
 }
