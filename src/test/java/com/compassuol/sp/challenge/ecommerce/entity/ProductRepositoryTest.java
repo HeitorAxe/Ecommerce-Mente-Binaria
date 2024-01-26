e1d4bb551913a9ddf167abfe28591beb5b7e5f26
HeitorAxe <heitorotieh8@gmail.com>
2024-01-26 01:34:17 -0300

    test: adicionando testes relacionados à listagem de produtos

--- src/test/java/com/compassuol/sp/challenge/ecommerce/entity/ProductRepositoryTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/entity/ProductRepositoryTest.java
@@ -2,15 +2,22 @@ package com.compassuol.sp.challenge.ecommerce.entity;
 
 import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
 import com.compassuol.sp.challenge.ecommerce.product.repository.ProductRepository;
+import com.compassuol.sp.challenge.ecommerce.product.repository.projection.ProductProjection;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.jdbc.Sql;
 
-import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.INVALID_PRODUCT;
-import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.PRODUCT;
+import java.util.List;
+
+import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.*;
 
 @DataJpaTest
 public class ProductRepositoryTest {
@@ -41,5 +48,36 @@ public class ProductRepositoryTest {
 
         Assertions.assertThatThrownBy(() -> repository.save(emptyProduct)).isInstanceOf(RuntimeException.class);
         Assertions.assertThatThrownBy(() -> repository.save(invalidProduct)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Sql(scripts = "/sql/import_products.sql")
+    @Test
+    public void listAllProductsAsPage_ReturnsAllProductsProjections(){
+        Page<ProductProjection> response = repository.findAllAsProjection(PAGEABLE);
+        Assertions.assertThat(response).isNotEmpty();
+        Assertions.assertThat(response.getContent().get(0).getName()).isEqualTo("Televisão");
+    }
+
+
+    @Test
+    public void listAllProductsAsPage_ReturnsNoProductsProjections(){
+        Page<ProductProjection> response = repository.findAllAsProjection(PAGEABLE);
+        Assertions.assertThat(response).isEmpty();
+    }
+
+    @Sql(scripts = "/sql/import_products.sql")
+    @Test
+    public void listAllProducts_ReturnsAllProducts(){
+        List<Product> response = repository.findAll();
+        Assertions.assertThat(response).isNotEmpty();
+        Assertions.assertThat(response.get(0).getName()).isEqualTo("Televisão");
+    }
+
+
+    //@Sql(scripts = "/sql/import_products.sql")
+    @Test
+    public void listAllProducts_ReturnsNoProducts(){
+        List<Product> response = repository.findAll();
+        Assertions.assertThat(response).isEmpty();
     }
 }
