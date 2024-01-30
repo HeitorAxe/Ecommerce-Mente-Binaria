b62a724ad9afca736cbfb5fd3fda216dfdc1f0c1
HeitorAxe <heitorotieh8@gmail.com>
2024-01-30 01:17:49 -0300

    fix: corrigindo erro ao criar um produto válido

--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
@@ -40,9 +40,11 @@ public class OrderService {
         order.setProducts(new ArrayList<>());
         addressRepository.save(order.getAddress());
         orderRepository.save(order);
-        for(OrderHasProduct orderHasProduct: order.getProducts()){
+        for(OrderHasProduct orderHasProduct: products){
             orderHasProduct.setOrder(order);
-            orderHasProduct.setProduct(productRepository.findById(orderHasProduct.getProduct().getId()).orElseThrow());
+            orderHasProduct.setProduct(productRepository.findById(orderHasProduct.getProduct().getId()).orElseThrow(
+                    () -> new RuntimeException("Produto não encontrado")
+            ));
             order.getProducts().add(orderHasProduct);
         }
         orderRepository.save(order);
