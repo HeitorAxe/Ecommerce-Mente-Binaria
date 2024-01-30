894c5e1c4ab53dface53dd98b5a9b7de34130168
HeitorAxe <heitorotieh8@gmail.com>
2024-01-29 21:24:40 -0300

    feat: adicionando criação de lista de produtos à criação de pedidos

--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
@@ -8,6 +8,8 @@ import com.compassuol.sp.challenge.ecommerce.order.entity.OrderHasProduct;
 import com.compassuol.sp.challenge.ecommerce.order.repository.AddressRepository;
 import com.compassuol.sp.challenge.ecommerce.order.repository.OrderHasProductRepository;
 import com.compassuol.sp.challenge.ecommerce.order.repository.OrderRepository;
+import com.compassuol.sp.challenge.ecommerce.product.dto.mapper.ProductMapper;
+import com.compassuol.sp.challenge.ecommerce.product.repository.ProductRepository;
 import com.compassuol.sp.challenge.ecommerce.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,13 +26,19 @@ public class OrderService {
     private final AddressRepository addressRepository;
     private final OrderHasProductRepository orderHasProductRepository;
     private final ProductService productService;
-
+    private final ProductRepository productRepository;
     @Transactional
     public OrderResponseDTO createOrder(OrderCreateDTO createDto) {
         Order order = OrderMapper.toOrder(createDto);
         List<OrderHasProduct> products = new ArrayList<>(order.getProducts());
         order.setProducts(new ArrayList<>());
         addressRepository.save(order.getAddress());
+        orderRepository.save(order);
+        for(OrderHasProduct orderHasProduct:products){
+            orderHasProduct.setOrder(order);
+            orderHasProduct.setProduct(productRepository.findById(orderHasProduct.getProduct().getId()).orElseThrow());
+            order.getProducts().add(orderHasProduct);
+        }
         orderRepository.save(order);
         return OrderMapper.toDTO(order);
     }
