a093eeeeeefb1e26662be4e5cccae6f8a81c3950
HeitorAxe <heitorotieh8@gmail.com>
2024-01-30 16:44:14 -0300

    refactor: eliminando entidade OrderHasProduct

--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/dto/mapper/OrderMapper.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/order/dto/mapper/OrderMapper.java
@@ -1,8 +1,10 @@
 package com.compassuol.sp.challenge.ecommerce.order.dto.mapper;
 
 import com.compassuol.sp.challenge.ecommerce.order.dto.*;
+import com.compassuol.sp.challenge.ecommerce.order.entity.Address;
 import com.compassuol.sp.challenge.ecommerce.order.entity.Order;
 import com.compassuol.sp.challenge.ecommerce.order.entity.OrderHasProduct;
+import com.compassuol.sp.challenge.ecommerce.order.enums.PaymentMethod;
 import com.compassuol.sp.challenge.ecommerce.product.service.ProductService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
@@ -15,15 +17,13 @@ import static org.apache.commons.io.IOUtils.skip;
 public class OrderMapper {
     ProductService productService;
 
-    public static Order toOrder(OrderCreateDTO dto) {
+    public static Order toOrder(OrderCreateDTO dto, Order order) {
         ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration().setSkipNullEnabled(true);
-        mapper.addMappings(new PropertyMap<OrderHasProductDTO, OrderHasProduct>() {
-            protected void configure() {skip(destination.getId());}
-        });
-        Order order = mapper.map(dto, Order.class);
+        order.setAddress(mapper.map(dto.getAddress(), Address.class));
+        order.setPaymentMethod(PaymentMethod.valueOf(dto.getPaymentMethod()));
         return order;
     }
+
 
     public static OrderResponseDTO toDTO(Order order) {
         ModelMapper mapper = new ModelMapper();
@@ -31,16 +31,9 @@ public class OrderMapper {
         return mapper.map(order, OrderResponseDTO.class);
     }
 
-
-    public static Order toCancelOrder(OrderDeleteDTO dto) {
-        ModelMapper mapper = new ModelMapper();
-        mapper.addMappings(new PropertyMap<OrderHasProductDTO, OrderHasProduct>() {
-            protected void configure() {skip(destination.getId());}
-        });
-        return mapper.map(dto, Order.class);
-    }
     public static OrderResponseDeleteDTO toDtoDelete(Order order) {
         ModelMapper mapper = new ModelMapper();
+        //contar produtos na lista
         return mapper.map(order, OrderResponseDeleteDTO.class);
     }
 
--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/repository/OrderHasProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.compassuol.sp.challenge.ecommerce.order.repository;
-
-import com.compassuol.sp.challenge.ecommerce.order.entity.OrderHasProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderHasProductRepository extends JpaRepository<OrderHasProduct, Long> {
-}
