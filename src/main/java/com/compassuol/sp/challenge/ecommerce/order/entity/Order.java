1d7d9a13e79af8f25c3cf61c0aed29fdcafc35d6
HeitorAxe <heitorotieh8@gmail.com>
2024-01-30 23:31:55 -0300

    feat: adicionando calculo de valor total e refatoração em service

--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/entity/Order.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/order/entity/Order.java
@@ -1,7 +1,9 @@
 package com.compassuol.sp.challenge.ecommerce.order.entity;
 
+import com.compassuol.sp.challenge.ecommerce.order.dto.mapper.OrderMapper;
 import com.compassuol.sp.challenge.ecommerce.order.enums.OrderStatus;
 import com.compassuol.sp.challenge.ecommerce.order.enums.PaymentMethod;
+import com.compassuol.sp.challenge.ecommerce.order.exception.OrderStatusNotAuthorizedException;
 import com.compassuol.sp.challenge.ecommerce.product.entity.Product;
 import jakarta.persistence.*;
 import lombok.*;
@@ -11,6 +13,9 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.compassuol.sp.challenge.ecommerce.order.enums.OrderStatus.CANCELED;
+import static com.compassuol.sp.challenge.ecommerce.order.enums.OrderStatus.CONFIRMED;
 
 @Getter
 @Setter
@@ -43,7 +48,7 @@ public class Order implements Serializable {
     private Double totalValue;
 
     @Column(name = "subtotal_value")
-    private Double subTotalValue;
+    private Double subTotalValue = 0.0;
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
@@ -63,6 +68,33 @@ public class Order implements Serializable {
 
     @Column(name = "cancel_reason")
     private  String cancelReason;
+
+    public void addProduct(Product product, int quantity){
+        for (int i =0 ; i<quantity; i++){
+            this.getProducts().add(product);
+            this.subTotalValue+=product.getPrice();
+        }
+        totalValue = subTotalValue;
+        if(paymentMethod==PaymentMethod.PIX)
+            totalValue-=totalValue/100.0 * 5.0;
+    }
+
+    public void cancel(String cancelReason){
+        LocalDateTime cancelationPeriod = this.getCreationDate().plusDays(90);
+        if(this.orderStatus == OrderStatus.CONFIRMED && LocalDateTime.now().isBefore(cancelationPeriod)){
+            this.orderStatus = CANCELED;
+            this.cancelationDate = LocalDateTime.now();
+            this.cancelReason = cancelReason;
+        }else if (this.orderStatus != CONFIRMED){
+            throw new OrderStatusNotAuthorizedException(
+                    "O status do pedido deve ser diferente de SENT"
+            );
+        }else if (LocalDateTime.now().isAfter(cancelationPeriod)){
+            throw new OrderStatusNotAuthorizedException(
+                    "O cancelamento do pedido só pode ser feito antes de 90 dias da compra"
+            );
+        }
+    }
 
 
 
--- src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderService.java
@@ -37,8 +37,7 @@ public class OrderService {
         Order order = new Order();
         OrderMapper.toOrder(createDto, order);
         for(OrderHasProductDTO orderProduct : createDto.getProducts())
-            for(int i = 0; i<orderProduct.getQuantity(); i++)
-                order.getProducts().add(productRepository.findById(orderProduct.getProductId()).orElseThrow());
+            order.addProduct(productRepository.findById(orderProduct.getProductId()).orElseThrow(), orderProduct.getQuantity());
         ViaCepResponseDTO viaCepResponse = viaCepConsumerFeign.getAddressByPostalCode(order.getAddress().getPostalCode());
         ViaCepResponseMapper.complementAddress(viaCepResponse, order.getAddress());
         order.setCreationDate(LocalDateTime.now());
@@ -60,26 +59,10 @@ public class OrderService {
     }
 
     @Transactional
-    public OrderResponseDeleteDTO removeOrder(Long id, OrderDeleteDTO deleteDto) {
-        Order order = orderRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException(String.format("Order id %d not found", id))
-        );
-        LocalDateTime purchasePeriod = order.getCreationDate().plusDays(90);
-        if (order.getOrderStatus() == CONFIRMED && LocalDateTime.now().isBefore(purchasePeriod)){ // Se agora for antes da data limite do cancelamento
-                order.setOrderStatus(CANCELED);
-                order.setCancelationDate(LocalDateTime.now());
-                order.setCancelReason(deleteDto.getCancelReason());
-        } else if (order.getOrderStatus() != CONFIRMED) {
-            throw new OrderStatusNotAuthorizedException(
-                    "O status do pedido deve ser diferente de SENT"
-            );
-        }else if (LocalDateTime.now().isAfter(purchasePeriod)){
-            throw new OrderStatusNotAuthorizedException(
-                    "O cancelamento do pedido só pode ser feito antes de 90 dias da compra"
-            );
-        }
-        return OrderMapper.toDtoDelete(order);
-
+    public OrderResponseDTO removeOrder(Long id, OrderDeleteDTO deleteDto) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(String.format("Order id %d not found", id)));
+        order.cancel(deleteDto.getCancelReason());
+        return OrderMapper.toDTO(order);
     }
 
     public OrderResponseDTO updateOrder(Long id, OrderUpdateDTO orderDto) {
