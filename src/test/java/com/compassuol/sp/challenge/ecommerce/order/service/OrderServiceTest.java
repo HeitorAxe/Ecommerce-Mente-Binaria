7764d9e76463bee9e25e26726d47ad805c6a5b23
pablitohaddad <pablohaddad73@gmail.com>
2024-01-31 10:00:16 -0400

    test: terminando a estruturação dos testes e adicionando o primeiro teste de delete order

--- src/test/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderServiceTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/order/service/OrderServiceTest.java
@@ -1,11 +1,40 @@
 package com.compassuol.sp.challenge.ecommerce.order.service;
 
+import com.compassuol.sp.challenge.ecommerce.common.ProductConstants;
+import com.compassuol.sp.challenge.ecommerce.order.dto.OrderCreateDTO;
+import com.compassuol.sp.challenge.ecommerce.order.dto.OrderDeleteDTO;
+import com.compassuol.sp.challenge.ecommerce.order.dto.OrderResponseDTO;
+import com.compassuol.sp.challenge.ecommerce.order.dto.mapper.OrderMapper;
+import com.compassuol.sp.challenge.ecommerce.order.entity.Order;
+import com.compassuol.sp.challenge.ecommerce.order.enums.OrderStatus;
+import com.compassuol.sp.challenge.ecommerce.order.exception.OrderStatusNotAuthorizedException;
+import com.compassuol.sp.challenge.ecommerce.order.repository.OrderRepository;
+import com.compassuol.sp.challenge.ecommerce.product.dto.ProductResponseDTO;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.ui.ModelMap;
 
+import java.util.Optional;
+
+import static com.compassuol.sp.challenge.ecommerce.common.OrderConstants.ORDER_WITH_STATUS_CONFIRMED;
+import static com.compassuol.sp.challenge.ecommerce.common.OrderConstants.ORDER_WITH_STATUS_SENT;
+import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.PRODUCT_CREATE_DTO;
+import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.PRODUCT_RESPONSE_DTO;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
 
+    @InjectMocks
+    OrderService orderService;
+    @Mock
+    OrderRepository orderRepository;
     @Test
     void createOrder() {
     }
@@ -19,8 +48,23 @@ class OrderServiceTest {
     }
 
     @Test
-    void removeOrder() {
+    void removeProduct_WithValidData_ReturnsOrderWithOrderStatusCanceled() {
+        OrderDeleteDTO delete = new OrderDeleteDTO("Não gostei do produto");
+        when(orderRepository.findById(ORDER_WITH_STATUS_CONFIRMED.getId())).thenReturn(Optional.of(ORDER_WITH_STATUS_CONFIRMED));
+        OrderResponseDTO sut = orderService.removeOrder(ORDER_WITH_STATUS_CONFIRMED.getId(), delete);
+        assertThat(sut.getOrderStatus()).isEqualTo(String.valueOf(OrderStatus.CANCELED));
+        assertThat(sut.getCancelationDate()).isNotNull();
+        assertThat(sut.getCancelReason()).isNotNull();
     }
+//    @Test
+//    void removeOrder_WithNotExistingId_doesNotThrowAnyException() {
+//        OrderDeleteDTO delete = new OrderDeleteDTO("");
+//        when(orderRepository.findById(ORDER_WITH_STATUS_SENT.getId())).thenReturn(Optional.of(ORDER_WITH_STATUS_SENT));
+//        OrderResponseDTO sut = orderService.removeOrder(ORDER_WITH_STATUS_SENT.getId(), delete);
+//        assertThat(sut.getOrderStatus()).isEqualTo(String.valueOf(OrderStatus.SENT));
+//        assertThat(sut.getCancelationDate()).isNull();
+//        assertThat(sut.getCancelReason()).isNull();
+//    }
 
     @Test
     void updateOrder() {
